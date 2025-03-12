1bbbb165dd15df1c4f848defb63f2d36eeeffaf7
Oxnack <128256361+Oxnack@users.noreply.github.com>
2025-03-12 19:21:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
-TOKEN = 'your'
+TOKEN = 'bot_token'
 
-bot_name = "name"
+bot_name = "bot_name"
 
 #https://t.me/your_bot?start=parameter 
 
 reqestPass_hash = "ac09723b8aa6cbc0017cec20523855d87c2cd1ff85ac71e4f4daea0f3e89f266" #897jkgy2489hei
 
 # db data: ------------------------------------->>>
-host = 'hostaddr'  
+host = 'host_addr'  
 database = 'tg_bot_data'
 table_name_users = "users"
 table_name_messages = "messages"
 
-user = 'postgres'
-password = '0000'
+user = 'name'
+password = 'passw'
 
 # rules: root(send, get_admin, get_manager), admin(send, get_manager), manager(send), user(not)
 
